b579c6ea8cd1ea4be0104d241d9d9363b80fde83
christoph-codes <tkcwebdev@gmail.com>
2023-01-17 23:13:54 -0800

    7: Create menu page. This resolves issue #7 & #12

---
+++ site/pages/menu/index.tsx
@@ -0,0 +1,31 @@
+import Button from "@/components/Button";
+import HighlightCards from "@/components/HighlightCards";
+import PageHeader from "@/components/PageHeader";
+import Tabs from "@/components/Tabs";
+import PageTemplate from "@/templates/Page";
+import { menu } from "@/utils/menu";
+import { toastTabLink } from "@/utils/navLinks";
+import { Flex } from "@chakra-ui/react";
+import styles from "./Menu.module.scss";
+
+const Menu = () => {
+	return (
+		<PageTemplate
+			className={styles.Menu}
+			metaTitle="Menu » Grur's Sports Bar & Grill"
+			metaDescription="View our menu and our amazing selection of our signature dishes for Guru's Sports Bar & Grill located in St. George, Utah"
+		>
+			<PageHeader
+				title="Menu"
+				description="View our menu and our amazing selection of our signature dishes for Guru's Sports Bar & Grill located in St. George, Utah"
+			/>
+			<Tabs content={menu} className={styles.Menu__tabs} />
+			<Flex justifyContent="center" paddingBottom="64px">
+				<Button href={toastTabLink}>Order Now</Button>
+			</Flex>
+			<HighlightCards />
+		</PageTemplate>
+	);
+};
+
+export default Menu;
