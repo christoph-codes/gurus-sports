91f3f40349c3ff7a8cb78a7d4855d3e65d333f6d
Christopher Jones <tkcwebdev@gmail.com>
2023-01-19 19:58:17 -0800

    11: Create individual position page

--- site/pages/careers/[position].tsx
+++ site/pages/careers/[position].tsx
@@ -1,3 +1,40 @@
+import PageTemplate from "@/templates/Page";
+import { useRouter } from "next/router";
+import { positions } from "@/utils/positions";
+import PageHeader from "@/components/PageHeader";
+import Title from "@/components/Title";
+import { Flex, Text } from "@chakra-ui/react";
+import Button from "@/components/Button";
+import styles from "./Position.module.scss";
+import Form from "@/components/Form";
+
+const Position = () => {
+	const router = useRouter();
+	const { position } = router.query;
+	const job = positions.find((j) => j.slug === position);
+	const submitApplication = () => {
+		console.log("submitApplication");
+	};
+	if (job) {
+		return (
+			<PageTemplate
+				metaTitle={`${job.title} » Career Opportunities with Guru's Sports Bar & Grill`}
+				metaDescription={job.shortDescription}
+				className={styles.Position}
+			>
+				<PageHeader
+					title={job.title}
+					description={job.shortDescription}
+				/>
+				<Title h3 textAlign="center">
+					Job Description
+				</Title>
+				<Text textAlign="center" marginBottom="32px">
+					{job.longDescription}
+				</Text>
+				<Flex justifyContent="center">
+					<Button href="#apply">Apply Now</Button>
+				</Flex>
 				{/** TODO: #25 ADD FORM HERE */}
 				<Form
 					formName={`position-apply-${job.title}`}
@@ -46,3 +83,10 @@
 						},
 					]}
 				/>
+			</PageTemplate>
+		);
+	}
+	return null;
+};
+
+export default Position;
