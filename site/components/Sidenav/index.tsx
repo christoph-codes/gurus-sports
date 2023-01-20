2b606aa5162eb5679d4dc48b91abc0662d63ede5
Christopher Jones <tkcwebdev@gmail.com>
2023-01-19 22:33:27 -0800

    11: Add indicator for navlinks for current page for sidenav

--- site/components/Sidenav/index.tsx
+++ site/components/Sidenav/index.tsx
@@ -9,6 +9,7 @@ import {
 } from "../../utils/navLinks";
 import styles from "./Sidenav.module.scss";
 import NavLink from "../NavLink";
+import { useRouter } from "next/router";
 
 export type TSideNav = {
 	isOpen?: boolean;
@@ -22,50 +23,64 @@ const SideNav: FC<TSideNav> = ({
 	toggleSideNav,
 	className,
 	...rest
-}) => (
-	<aside
-		className={`${styles.SideNav} ${className} ${
-			isOpen ? styles.SideNav__isOpen : ""
-		}`}
-		{...rest}
-	>
-		<button
-			className={styles.SideNav__close}
-			type="button"
-			onClick={() => toggleSideNav()}
+}) => {
+	const { pathname } = useRouter();
+	return (
+		<aside
+			className={`${styles.SideNav} ${className} ${
+				isOpen ? styles.SideNav__isOpen : ""
+			}`}
+			{...rest}
 		>
-			<CloseIcon boxSize={8} color="currentColor" />
-		</button>
-		{navigationLinks?.map((item, index) => (
-			<NavLink
-				className={`${styles.SideNav__link} ${
-					item.cta && styles.SideNav__CTA
-				}`}
-				key={index}
-				href={item.link.path}
+			<button
+				className={styles.SideNav__close}
+				type="button"
 				onClick={() => toggleSideNav()}
-				cta={item.cta}
 			>
-				{item.link.label}
-			</NavLink>
-		))}
-		<Flex justifyContent="center">
-			<NavLink href={facebookLink}>
-				<Icon
-					_hover={{ color: "brand.secondary.hover" }}
-					boxSize={8}
-					as={FaFacebook}
-				/>
-			</NavLink>
-			<NavLink href={instagramLink}>
-				<Icon
-					_hover={{ color: "brand.secondary.hover" }}
-					boxSize={8}
-					as={FaInstagram}
-				/>
-			</NavLink>
-		</Flex>
-	</aside>
-);
+				<CloseIcon boxSize={8} color="currentColor" />
+			</button>
+			{navigationLinks?.map((item, index) => {
+				const isLinkActive = pathname === item.link.path;
+				return (
+					<NavLink
+						key={index}
+						href={item.link.path}
+						onClick={() => toggleSideNav()}
+						cta={item.cta}
+						className={`${
+							!item.cta
+								? styles[
+										`SideNav__link${
+											isLinkActive ? "--active" : ""
+										}`
+								  ]
+								: ""
+						} ${styles.SideNav__link} ${
+							item.cta && styles.SideNav__CTA
+						}`}
+					>
+						{item.link.label}
+					</NavLink>
+				);
+			})}
+			<Flex justifyContent="center">
+				<NavLink href={facebookLink}>
+					<Icon
+						_hover={{ color: "brand.secondary.hover" }}
+						boxSize={8}
+						as={FaFacebook}
+					/>
+				</NavLink>
+				<NavLink href={instagramLink}>
+					<Icon
+						_hover={{ color: "brand.secondary.hover" }}
+						boxSize={8}
+						as={FaInstagram}
+					/>
+				</NavLink>
+			</Flex>
+		</aside>
+	);
+};
 
 export default SideNav;
