6e1d74c08a623b66152b6d80bdf8d359684e29a4
Christopher Jones <tkcwebdev@gmail.com>
2023-02-03 10:04:32 -0800

    33: Create new structure for servers page and add new content

--- site/utils/positions/_server.ts
+++
@@ -1,6 +0,0 @@
-export const server = {
-    slug: 'server',
-    title: 'Server',
-    shortDescription: 'We are seeking servers with experience taking orders, serving food and drinks, and providing excellent customer service.',
-    longDescription: 'We are seeking servers for our sports bar and grill located in St. George, Utah. Responsibilities include taking orders, serving food and drinks, and providing excellent customer service. Candidates should have prior experience in a fast-paced restaurant environment and a strong understanding of food and beverage service. Must be able to work evenings, weekends and holidays. If you are a team player, have a great attitude and are looking for a fun and exciting work environment, please apply today!'
-}
---
+++ site/utils/positions/_servers.ts
@@ -0,0 +1,26 @@
+export const servers = {
+    slug: "servers",
+	title: "Servers",
+	shortDescription:
+		"The Bartender is responsible for providing exceptional customer service and creating a positive atmosphere by mixing and serving drinks to guests in the bar. This role requires a high level of product knowledge, attention to detail, and excellent customer service skills.",
+	responsibilities: [
+		"Greet guests as they arrive and provide menu recommendations",
+        "Take and accurately input food and drink orders into the point-of-sale system",
+        "Serve food and drinks to guests in a prompt and professional manner",
+        "Respond to guest concerns or complaints in a professional and courteous manner",
+        "Ensure that guests receive the correct order and that all items are presented in an attractive manner",
+        "Follow all safety and sanitation policies and procedures",
+        "Accurately process transactions using a point-of-sale system",
+        "Perform opening and closing duties as required",
+	],
+	requirements: [
+		"Prior server experience is preferred",
+        "Excellent customer service skills",
+        "Strong communication and interpersonal skills",
+        "Ability to multitask and work in a fast-paced environment",
+        "Ability to work well in a team-oriented environment",
+        "Must be able to stand for long periods and lift heavy items",
+	],
+	closing:
+		"We offer competitive pay and benefits, a supportive and positive work environment, and opportunities for growth and advancement. If you are passionate about providing great customer service and have a love for the hospitality industry, we want to hear from you!",
+};
