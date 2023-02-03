8149f2fc3fcd9496303b425079a3ca590f578685
Christopher Jones <tkcwebdev@gmail.com>
2023-02-03 10:35:53 -0800

    33: Flex start the button to be aligned to the top of the position title for spacing

--- site/utils/positions/index.ts
+++ site/utils/positions/index.ts
@@ -1,8 +1,10 @@
 import { bartenders } from './_bartenders';
+import { cooks } from './_cooks';
 import { dishwashers } from './_dishwashers';
 import { hosts } from './_hosts';
+import { managers } from './_managers';
 import { servers } from "./_servers";
 
 export const positions = [
-	hosts, servers, bartenders, dishwashers, 
+	managers, cooks, bartenders, hosts, servers,  dishwashers, 
 ];
