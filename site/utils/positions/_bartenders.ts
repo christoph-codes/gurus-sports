2bf1622e2fdad5cfc9d1fddc54efdeeb1e5619e0
Christopher Jones <tkcwebdev@gmail.com>
2023-02-03 10:05:14 -0800

    33: Create bartender position listing

---
+++ site/utils/positions/_bartenders.ts
@@ -0,0 +1,25 @@
+export const bartenders = {
+	slug: "bartenders",
+	title: "Bartenders",
+	shortDescription:
+		"The Bartender is responsible for providing exceptional customer service and creating a positive atmosphere by mixing and serving drinks to guests in the bar. This role requires a high level of product knowledge, attention to detail, and excellent customer service skills.",
+	responsibilities: [
+		"Prepare and serve a variety of alcoholic and non-alcoholic beverages to guests in a prompt and professional manner",
+        "Maintain a clean and organized bar area, including stocking supplies, washing glassware, and cleaning surfaces",
+        "Follow all safety and sanitation policies and procedures",
+        "Accurately process transactions using a point-of-sale system",
+        "Provide menu recommendations and answer any questions guests may have",
+        "Respond to guest concerns or complaints in a professional and courteous manner",
+        "Perform opening and closing duties as required",
+	],
+	requirements: [
+		"Prior bartending experience is preferred",
+        "Strong knowledge of a variety of drinks and drink preparation techniques",
+        "Excellent customer service skills",
+        "Ability to multitask and work in a fast-paced environment",
+        "Strong communication and interpersonal skills",
+        "Ability to work well in a team-oriented environment",
+	],
+	closing:
+		"Must be at least 21 years of age and able to obtain a food handler's permit and alcohol service certification We offer competitive pay and benefits, a supportive and positive work environment, and opportunities for growth and advancement. If you are passionate about providing great customer service and have a love for mixology, we want to hear from you!",
+};
