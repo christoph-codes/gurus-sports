6cb8001b087238d03b96fcdf7c9909fad23f2ace
Christopher Jones <tkcwebdev@gmail.com>
2023-02-18 13:58:54 -0800

    36: Add phone input validation

--- site/utils/inputValidations.ts
+++ site/utils/inputValidations.ts
@@ -2,6 +2,7 @@ export enum EErrorMessages {
 	"REQUIRED" = "This field is required",
 	"OVERFIVE" = "Value must be over five characters",
 	"EMAIL" = "Please enter a valid email",
+	"PHONE" = "Please enter a valid phone number including area code",
 }
 // All functions must either return true or the error message in which failed.
 const inputValidations = {
@@ -21,6 +22,12 @@ const inputValidations = {
 		const pattern = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
 		if (!pattern.test(value)) {
 			return EErrorMessages.EMAIL;
+		}
+		return "";
+	},
+	PHONE(value: any) {
+		if (value.length !== 10 || typeof value === 'number') {
+			return EErrorMessages.PHONE;
 		}
 		return "";
 	},
